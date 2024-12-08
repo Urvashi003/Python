b49855f149153d9ca29ef4ec36607425cd27198c
Urvashi003 <urvashibullani003@gmail.com>
2024-12-08 23:10:59 +0530

    first commit

---
+++ basic.py
@@ -0,0 +1,124 @@
+# print("hello")
+# age=22
+# age=24
+# print(age)
+
+# is_genius= True
+# print(is_genius)
+
+# name= input("what is your name?")
+# print(name)
+
+# #type conversion
+# old_age= input("what is your age: ")
+# new_age= int(old_age)+2
+# print(new_age)
+
+# name= "urvashi"
+# print(name.upper()) #copy bani thi
+# print(name)
+
+# print(name.find('s'))
+# print(name.replace("urvashi","superstar urvashi"))
+
+# #keywords
+# print('i' in name)
+
+# #arithematic operator
+# print(5-2) 
+# print(3==3)
+
+# age=1
+# if age>=18:
+#     print("vote")
+#     print("karo")
+# elif age<18 and age>3:
+#     print("time hai")  
+# else:
+#     print("nono")   
+# print("thanks")       
+
+# first= input("enter first no: ")
+# operator = input("enter operator(+,-,*,/,%): ")
+# second= input("enter second no: ")
+# first= int(first)
+# second= int(second)
+# if operator== '+':
+#     print(first+second)
+# elif operator== '-':
+#     print(first-second)   
+# elif operator== '*':
+#     print(first-second)  
+# elif operator== '/':
+#     print(first-second)  
+# elif operator== '%':
+#     print(first-second)
+# else:
+#     print("invalid operator")  
+    
+    
+# ans=range(8)
+# print(ans)
+
+#loops
+
+# i=1
+# while i<=5:
+#     print(i)
+#     i+=1
+
+
+# for i in range(8):
+#     print(i) 
+
+
+#lists- complex data type, collection of items
+# marks= [95,98,97,91,88,99]
+# print(marks[0]) 
+
+# print(marks[-1])
+# print(marks[0:2])
+
+# marks.append(90) #append last mein hoga
+# marks.insert(0,99)
+# print(marks)
+
+# print(99 in marks)
+# print(len(marks))
+# marks.clear()
+
+# for s in marks:
+#     if s== "91":
+#       break
+# print(s)
+
+# marks= (98,90,90)
+# # marks[0]=88 #error
+
+# print(marks.count(90))
+# print(marks.index(90))
+
+#set- no index
+# ma= {9,0,8,2}
+# for i in ma:
+#     print(i)
+
+
+#dictionary
+# marks= {"eng": 98, "che" : 99}
+# info = {"ram": "bal"}
+
+# print(marks["che"])
+# marks["phy"]= 88
+# print(marks)
+
+
+# def fun_name(parameters):
+#     #do something
+
+
+# def print_sum(f,s):
+#     print(f+s)
+
+# print_sum(9,7)    
+  
